639377708e5e78bb9a6402b7b44598ab8fee6282
sontx <xuanson33bk@gmail.com>
2016-11-22 00:44:04 +0700

    + setup both sides

---
+++ jade-agent-system/src/com/blogspot/sontx/jade/agentsystem/client/ClientProgram.java
@@ -0,0 +1,10 @@
+package com.blogspot.sontx.jade.agentsystem.client;
+
+public class ClientProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ jade-agent-system/src/com/blogspot/sontx/jade/agentsystem/server/ServerProgram.java
@@ -0,0 +1,8 @@
+package com.blogspot.sontx.jade.agentsystem.server;
+
+public class ServerProgram {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
