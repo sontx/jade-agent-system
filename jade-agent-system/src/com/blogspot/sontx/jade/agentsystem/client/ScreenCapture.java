92f1457c99998959db32150f4b25f7d61ffdf793
trongvn13 <vonhutrong@gmail.com>
2016-11-22 16:13:02 +0700

    + add feature capturing screen

---
+++ jade-agent-system/src/com/blogspot/sontx/jade/agentsystem/client/ScreenCapture.java
@@ -0,0 +1,36 @@
+package com.blogspot.sontx.jade.agentsystem.client;
+
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+/**
+ * @author trongvn13
+ *
+ * Nov 22, 2016 - 4:03:52 PM
+ */
+public class ScreenCapture {
+	public static void capture(String filePath) {
+		capture(filePath, "png");
+	}
+	
+	public static void capture(String filePath, String fileType) {
+		Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+		BufferedImage capture;
+		try {
+			capture = new Robot().createScreenCapture(screenRect);
+			File file = new File(filePath);
+			ImageIO.write(capture, fileType, file);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
